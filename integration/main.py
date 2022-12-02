4ed2bc244c165ed66f0b49fdbd7f8b1bce1f5fff
Ivan Nunez <ivanluis.cba@gmail.com>
2022-12-01 23:30:49 -0300

    Split integration tests

--- integration/main.py
+++ integration/main.py
@@ -16,12 +16,24 @@ class TestFrontend(unittest.TestCase):
         self.uri = os.getenv('FRONTEND_URI', 'http://localhost:3000')
         self.addCleanup(self.browser.quit)
 
-    def test_index(self):
+    def test_index_title(self):
         self.browser.get(self.uri)
         self.assertEqual(self.browser.title, 'Voting App')
+
+    def test_index_vote_a(self):
+        self.browser.get(self.uri)
         time.sleep(2)
         btn_a = self.browser.find_element(By.ID, 'btn-vote-a')
         btn_a.click()
+        time.sleep(2)
+        lbl_success = self.browser.find_element(By.ID, 'lbl-success')
+        assert lbl_success.text == 'Success!'
+
+    def test_index_vote_b(self):
+        self.browser.get(self.uri)
+        time.sleep(2)
+        btn_b = self.browser.find_element(By.ID, 'btn-vote-b')
+        btn_b.click()
         time.sleep(2)
         lbl_success = self.browser.find_element(By.ID, 'lbl-success')
         assert lbl_success.text == 'Success!'
